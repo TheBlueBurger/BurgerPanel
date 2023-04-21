a38d4ebd1d051230bd830310df4cdbd04418a201
TheBlueBurger <58081413+TheBlueBurger@users.noreply.github.com>
2023-04-21 19:00:01 +0200

    Update isValidMCVersion.ts

--- Server/src/util/isValidMCVersion.ts
+++ Server/src/util/isValidMCVersion.ts
@@ -1,4 +1,13 @@
-export default async function isValidMCVersion(version: string): Promise<boolean> {
+let cachedManifest: unknown;
+let manifestCachedAt = Date.now();
+export async function getManifest() {
+    if(Date.now() > (manifestCachedAt + 30*60*1000) && typeof cachedManifest != "undefined") return cachedManifest; // 30 mins cache
     let manifest = await (await fetch("https://launchermeta.mojang.com/mc/game/version_manifest.json")).json();
+    if(!manifest?.versions) throw new Error("Mojang broke? manifest.versions doesnt exist");
+    cachedManifest = manifest;
+    return manifest;
+}
+export default async function isValidMCVersion(version: string): Promise<boolean> {
+    let manifest = await getManifest();
     return manifest?.versions.some((v: any) => v.id == version);
 }
