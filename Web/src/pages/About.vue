221f64b56df0423e6bb2bb9307d71e1b655bee60
TheBlueBurger <58081413+TheBlueBurger@users.noreply.github.com>
2023-05-21 21:37:39 +0200

    haha oops

--- Web/src/pages/About.vue
+++ Web/src/pages/About.vue
@@ -5,6 +5,6 @@
     <p><a href="https://github.com/TheBlueBurger/BurgerPanel">Github repo</a></p>
     <h3>Credits</h3>
     <p>- <a href="https://github.com/TheBlueBurger" target="_blank">TheBlueBurger</a>: Most backend, most frontend, some CSS</p>
-    <p>- <a href="https://github.com/TheRedXD" target="_blank">_TheRedex</a>: Some css, notification system (the bad design is his fault!!!)</p>
+    <p>- <a href="https://github.com/TheRedXD" target="_blank">_TheRedex</a>: Some css, notification system</p>
     <p>- <a href="https://github.com/ArrayBuffer01" target="_blank">ArrayBuffer01</a>: Helping with backend and frontend</p>
 </template>
